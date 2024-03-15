dc696e2b2e1cf534735f6761f097b6c9d840dd7a
juan <juanm2405@gmail.com>
2024-03-14 21:08:43 -0500

    clase 14/03/24

---
+++ intro/operadores.js
@@ -0,0 +1,26 @@
+//Operadores aritmeticos basicos
+let counter = 100;
+
+//1. Suma
+
+counter = counter + 20;
+console.log(counter);
+counter +=20;
+console.log(counter);
+
+//2. Resta
+
+let numberA = 50;
+
+let rest = numberA - 20;
+console.log(rest);
+
+rest -=10;
+console.log(rest);
+
+
+//3. Division
+
+let numberB = 100;
+
+let division 
